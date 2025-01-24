1bc9292a3c4a9a4d2a4095572a83aea4220d3147
rezgu <rezgui012@gmail.com>
2025-01-24 21:45:38 +0100

    added crud and finished UI

---
+++ src/app/services/blog-posts.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+import { BlogPostsService } from './blog-posts.service';
+
+describe('BlogPostService', () => {
+  let service: BlogPostsService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(BlogPostsService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
