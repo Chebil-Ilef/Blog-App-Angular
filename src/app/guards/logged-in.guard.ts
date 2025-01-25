2ce3bd5994230ae451c47c64edf16f5f1ef6e79e
Mohamed Anas Neji <mohamedanas.neji@medius.com>
2025-01-25 09:12:15 +0100

    add error page

--- src/app/guards/logged-in.guard.ts
+++ src/app/guards/logged-in.guard.ts
@@ -2,17 +2,17 @@ import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { inject } from '@angular/core';
 import { map } from 'rxjs';
-export const authGuard: CanActivateFn = (route, state) => {
+
+export const loggedInGuard: CanActivateFn = (route, state) => {
   let authService : AuthService = inject(AuthService)
   let router : Router = inject(Router)
   return authService.loadUser().pipe(
     map(user=>{
-      if(user)
-        return true;
-      else{
-        router.navigate(['/dashboard'])
+      if(user){
+        router.navigate(['/dashboard/home'])
         return false;  
-      }
+      }else
+        return true;
     })
   );
 };
