e235fcce0f4c14b2d58d679b3895162403cb6bf2
Mohamed Anas Neji <mohamedanas.neji@medius.com>
2025-01-25 09:34:25 +0100

    reorganize repo structure and lazyload the dashboard module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,17 +5,7 @@ import { SingleCategoryComponent } from './pages/single-category/single-category
 import { SinglePostComponent } from './pages/single-post/single-post.component';
 import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 import { MainComponent } from './sections/main/main.component';
-import { DashboardComponent } from './sections/dashboard/dashboard.component';
-import { DashboardHomeComponent } from './pages/dashboard-home/dashboard-home.component';
-import { DashboardCategoriesComponent } from './pages/dashboard-categories/dashboard-categories.component';
-import { AllPostsComponent } from './pages/dashboard-posts/all-posts/all-posts.component';
-import { NewPostComponent } from './pages/dashboard-posts/new-post/new-post.component';
-import { LoginComponent } from './pages/auth/login/login.component';
-import { authGuard } from './guards/auth.guard';
-import { DashboardSubscribersComponent } from './pages/dashboard-subscribers/dashboard-subscribers.component';
-import { DashboardCommentsComponent } from './pages/dashboard-comments/dashboard-comments.component';
-import { loggedInGuard } from './guards/logged-in.guard';
-import { ErrorPageComponent } from './pages/error-page/error-page.component';
+import { ErrorPageComponent } from './shared-components/error-page/error-page.component';
 
 const routes: Routes = [
   { path : '', component : MainComponent, children : [
@@ -24,16 +14,8 @@ const routes: Routes = [
     { path: 'post/:categoryId/:id', component : SinglePostComponent },
     { path: 'contact', component : ContactUsComponent}
   ]},
-  { path : 'dashboard', component: DashboardComponent , children :[
-    { path : '', component : LoginComponent, canActivate : [loggedInGuard]},
-    { path : 'home', component : DashboardHomeComponent, canActivate : [authGuard]},
-    { path : 'categories', component : DashboardCategoriesComponent, canActivate : [authGuard]},
-    { path : 'subscribers', component : DashboardSubscribersComponent, canActivate : [authGuard]},
-    { path : 'posts', component : AllPostsComponent, canActivate : [authGuard]},
-    { path : 'comments', component : DashboardCommentsComponent, canActivate : [authGuard]},
-    { path : 'posts/new', component : NewPostComponent, canActivate : [authGuard]},
-    { path : '**', component : DashboardComponent, canActivate : [authGuard]}
-  ]}
+  { path : 'dashboard', loadChildren: ()=> import('./dashboard/dashboard.module').then((m)=> m.DashboardModule)},
+  { path : '**', component : ErrorPageComponent}
 ];
 
 @NgModule({
