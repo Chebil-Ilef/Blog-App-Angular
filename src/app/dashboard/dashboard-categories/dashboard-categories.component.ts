f7e28f17febc960f7dbe653a1b5d43d62e53c53e
neirezcher <neirez.cherni@etudiant-fst.utm.tn>
2025-01-26 15:23:27 +0100

    Changed change detection strategy to OnPush

--- src/app/dashboard/dashboard-categories/dashboard-categories.component.ts
+++ src/app/dashboard/dashboard-categories/dashboard-categories.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, inject } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { Category } from 'src/app/Models/Category.model';
@@ -8,23 +8,20 @@ import { CategoriesService } from 'src/app/services/categories.service';
 @Component({
   selector: 'app-dashboard-categories',
   templateUrl: './dashboard-categories.component.html',
-  styleUrls: ['./dashboard-categories.component.css']
+  styleUrls: ['./dashboard-categories.component.css'],
+  changeDetection : ChangeDetectionStrategy.OnPush
 })
-
-
 export class DashboardCategoriesComponent {
+
 
   categories$: Observable<CategoryWithId[]>;
   formCategory:string = ""
   formStatus: string = "Add"
   categoryId: string = ""
   categoryService : CategoriesService = inject(CategoriesService)
-
-
   constructor() {
     this.categories$ = this.categoryService.loadData();
   }
-
 
   onSubmit(formData : NgForm){
     let categoryData : Category = {
@@ -42,14 +39,13 @@ export class DashboardCategoriesComponent {
     }
   }
 
-
   onEdit(categoryName : string, id: string){
     this.formCategory = categoryName
     this.formStatus = "Edit"
     this.categoryId=id
+
   }
 
-  
   onDelete(id : string){
     this.categoryService.deleteData(id)
   }
