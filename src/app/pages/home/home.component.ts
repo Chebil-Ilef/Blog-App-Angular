34c6a1095a339f1a81f0887e75ef6d02dcb46126
Mohamed Anas Neji <mohamedanas.neji@medius.com>
2025-02-01 14:13:24 +0100

    fix load more feature
    Co-authored-by: neirezcher <neirez.cherni@etudiant-fst.utm.tn>

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,21 +1,18 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { Observable, BehaviorSubject } from 'rxjs';
+import { Observable, BehaviorSubject, take, switchMap } from 'rxjs';
 import { BlogPostWithId } from 'src/app/Models/BlogPostWithId.model';
 import { BlogPostsService } from 'src/app/services/blog-posts.service';
 import { PostCardComponent } from '../../layouts/post-card/post-card.component';
 import { RouterLink } from '@angular/router';
 import { NgIf, NgFor, NgClass, AsyncPipe } from '@angular/common';
-import {DocumentSnapshot} from '@angular/fire/firestore'
+import {DocumentData, DocumentSnapshot} from '@angular/fire/firestore'
 import {
-  concatMap,
   map,
   scan,
   catchError,
   of,
   tap,
-  takeWhile,
 } from 'rxjs';
-import { Firestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-home',
@@ -38,44 +35,38 @@ export class HomeComponent {
   });
 
   totalPosts = 0;
-  
+
+  lastVisiblePost: DocumentSnapshot<DocumentData| DocumentData> | null = null;
+
+  totalPosts$ = this.blogPostsService.getTotalPostsCount().pipe(
+    map((response) => response.size),
+    take(1),
+    tap((response) => {this.totalPosts = response})
+  ).subscribe();
+
 // Observable for accumulating latest posts
 latestPosts$: Observable<BlogPostWithId[]> = this.paginationSubject.pipe(
-  takeWhile(
-    ({ lastVisibleDoc }) => lastVisibleDoc !== null,
-    true
+  switchMap(({ limit, lastVisibleDoc }) =>
+    this.blogPostsService.getLatestPosts(limit, lastVisibleDoc)
   ),
-  concatMap(({ limit, lastVisibleDoc }) =>
-    this.blogPostsService.getLatestPosts(limit, lastVisibleDoc).pipe(
-      tap((data) => {
-        this.totalPosts = data.total;
-        console.log('Fetched posts:', data.posts);
-        console.log('Last visible document:', data.lastVisible);
-
-        // Update the lastVisibleDoc in the paginationSubject
-        if (data.lastVisible) {
-          this.paginationSubject.next({ limit, lastVisibleDoc: data.lastVisible });
-        }
-      }),
-      map((data) => ({ posts: data.posts, lastVisibleDoc: data.lastVisible })),
-      catchError(() => of({ posts: [], lastVisibleDoc: null }))
-    )
-  ),
+  tap((data) => {
+      this.lastVisiblePost = data.lastVisible
+  }),
+  map((data) => data.posts),
   scan(
-    (acc, { posts, lastVisibleDoc }) => {
-      console.log('Scan - lastVisibleDoc:', lastVisibleDoc); // Debugging log
+    (acc, posts) => {
       // Accumulate posts
       const newPosts = [...acc, ...posts];
       return newPosts;
     },
     [] as BlogPostWithId[]
-  )
+  ),
+  catchError(() => of([]))
 );
 
 loadMore(): void {
-  console.log('Load More button clicked'); // Debugging log
-  const { limit, lastVisibleDoc } = this.paginationSubject.value;
-  console.log('getLatestPosts - limit:', limit, 'lastVisibleDoc:', lastVisibleDoc);
+  const limit = this.paginationSubject.value.limit;
+  const lastVisibleDoc = this.lastVisiblePost;
   if (lastVisibleDoc !== null) {
     this.paginationSubject.next({ limit, lastVisibleDoc });
   }
